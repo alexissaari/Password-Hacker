fa7617d641c4c222274a4759027f12594a734d13
alexissaari <alexissaari@gmail.com>
2018-11-20 16:37:51 -0600

    Works
    Map does distributed things and sends one item to reducer.

--- MapReduceAlg1.java
+++ MapReduceAlg1.java
@@ -31,12 +31,27 @@ public class MapReduceAlg1 {
 				throws IOException {
 			StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\n");
 			String line = null;
+			String password = "abab";
+			int counter = 0;
+			String guesses = "";
 			
 			while (tokenizer.hasMoreTokens()) {
 				line = tokenizer.nextToken();
 				
-				//Sending line to Reducer without any sorting or parsing
-				output.collect(line, password);
+				//output.collect(new Text(line), new Text(password));
+				
+				// Sending line to Reducer
+//				guesses += line + " ";
+//				counter++;
+//				if (counter == 1000) {
+//					output.collect(new Text(guesses), new Text(password));
+//					guesses = "";
+//					counter = 0;
+//				}
+				
+				if (line.equals(password)) {
+					output.collection(new Text(line), new Text(password));
+				}
 				
 			}
 		}
@@ -51,24 +66,54 @@ public class MapReduceAlg1 {
 	 *         friends.
 	 */
 	public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-		public void reduce(Text key, Iterator<org.w3c.dom.Text> values, OutputCollector<Text, Text> output, Reporter reporter)
+		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
 			Text[] texts = new Text[2];
 			int index = 0;
 			while (values.hasNext()) {
-				texts[index++] = new Text(values.next());
+				texts[index] = new Text(values.next());
+				index++;
 			}
+
 			
-			String guess = texts[0].toString();
-			String password = texts[1].toString();
+			String[] guesses = texts[0].toString().split(" ");
+			String password = "abab";// texts[1].toString();
 			List<String> list = new LinkedList<String>();
-			
-			if (guess.equals(password)) {
-				output.collect(key, new Text("Password found is: " + password));
-				return;
-			} else {
-				output.collect(key, new Text(guess));
+
+			//comparing guesses list to password
+			for (String guess : guesses) {
+				if (guess.equals(password)) {
+					list.add(guess);
+				}
 			}
+			
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < list.size(); i++) {
+				sb.append(list.get(i));
+				if (i != list.size() - 1)
+					sb.append(" ");
+			}
+			output.collect(key, new Text(sb.toString()));
+
+		}
+	}
+
+	public static class MyTimer {
+		Long startTime;
+		Long endTime;
+		boolean isFound = false;
+
+		public void startTimer() {
+			startTime = System.currentTimeMillis();
+		}
+
+		public void endTimer() {
+			isFound = true;
+			endTime = System.currentTimeMillis();
+		}
+
+		public Long totalTime() {
+			return (long) ((endTime - startTime) / 1000.00);
 		}
 	}
 
@@ -80,10 +125,12 @@ public class MapReduceAlg1 {
 	 *             classes, names, keys, and values to. We then set our input and
 	 *             output files and run the job.
 	 */
+
 	public static void main(String[] args) throws Exception {
 		JobConf conf = new JobConf(MapReduceAlg1.class);
 		conf.setJobName("MainFriend");
 
+		MyTimer myTimer = new MyTimer();
 		conf.setMapperClass(Map.class);
 		conf.setReducerClass(Reduce.class);
 
@@ -96,6 +143,9 @@ public class MapReduceAlg1 {
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
+		myTimer.startTimer();
 		JobClient.runJob(conf);
+		myTimer.endTimer();
+		System.out.println("Time took: " + myTimer.totalTime().toString());
 	}
 }
